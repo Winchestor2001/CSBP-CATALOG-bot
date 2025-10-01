821cefab5216d043348373c204b21a6f64e0136a
winchestor2001 <sbexruz2001@gmail.com>
2025-10-01 21:32:58 +0500

    Update ALLOWED_HOSTS for catalog API

--- core/settings.py
+++ core/settings.py
@@ -25,7 +25,7 @@ SECRET_KEY = 'django-insecure-wg1#3os1+&-6_iyvq+nh6og(ihg#xcxvjf9x%1y-gcupe*mr4$
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = ["time2buy.api.dizel.online", "localhost", "127.0.0.1"]
+ALLOWED_HOSTS = ["csbp-catalog.api.dizel.online", "localhost", "127.0.0.1"]
 # CORS_ALLOWED_ORIGINS = [
 #     "https://casual-store.netlify.app",
 # ]
